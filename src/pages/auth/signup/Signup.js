869d748dcdaac033e38c89bf93b6540dc5f2795e
giddy11 <edoghotugiddy@gmail.com>
2024-06-07 00:08:39 +0100

    fixed the issue of form submit

--- src/pages/auth/signup/Signup.js
+++ src/pages/auth/signup/Signup.js
@@ -1,67 +1,49 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup'
-import * as yup from 'yup'
+import { DevTool } from '@hookform/devtools';
+import { userSchema } from '../../../validations/userValidation';
 
 
 function Signup () {
- const schema = yup.object().shape({
 
- FirstName: yup.string().required("Your first name is required"),
- lastName: yup.string().required("Your last name is required"),
- PhoneNumber: yup.number().min(11).max(11).required("Your phone Number is not correct"),
- email: yup.string().email().required("Your email is required"),
- officeAddress: yup.string().required("Your office address is required"),
- dateOfbirth: yup.number().integer().min(16).max(200).required("Your age must be 16 and above"),
- country: yup.string().required("Your country is required"),
- stateOforigin: yup.string().required("Your State is required"),
- password: yup.string().min(4).max(30).required(),
- confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Password dont match").required(),
-
-
-
-});
-
-const{register, handleSubmit, formState: {errors}} = useForm({
-  resolver: yupResolver(schema)
+const{register, handleSubmit, control, formState: {errors}} = useForm({
+  resolver: yupResolver(userSchema)
 });
   
-
  const onSubmit=(data)=>{
-      handleSubmit(data)
-
-
+      console.log(data)
     }
   return (
     <>
       <div>
-        <form >
-          <label>First Name: </label> <input type='text' placeholder="First Name..." {...register("First Name")}/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor='firstName'>First Name: </label> <input type='text' id='firstName' placeholder="First Name..." {...register("firstName")}/>
           
-          <p>{errors.FirstName?.message}</p>
+          <p>{errors.firstName?.message}</p>
           
-          <label>Last Name: </label> <input type='text' placeholder="Last Name..." {...register("Last Name")} />
+          <label htmlFor='lastName'>Last Name: </label> <input type='text' id='lastName' placeholder="Last Name..." {...register("lastName")} />
           
-          <p>{errors.LastName?.message}</p>
+          <p>{errors.lastName?.message}</p>
           
-          <label>Phone Number: </label> <input type='Number' placeholder="Phone Number..." {...register("Phone number")} />
+          <label>Phone Number: </label> <input type='Number' placeholder="Phone Number..." {...register("phoneNumber")} />
           
-          <p>{errors.PhoneNumber?.message}</p>
+          <p>{errors.phoneNumber?.message}</p>
           
-          <label>Email: </label> <input type='text' placeholder="Email..." {...register("Email")}/>
+          <label>Email: </label> <input type='text' id='email' placeholder="Email..." {...register("email")}/>
           
           <p>{errors.email?.message}</p>
           
-          <label>Office Address: </label> <input type='text' placeholder="Office address..." {...register("Office address")}/>
+          <label>Office Address: </label> <input type='text' placeholder="Office address..." {...register("officeAddress")}/>
          
           <p>{errors.officeAddress?.message}</p>
           
-          <label>Date Of Birth: </label><input type='Number' placeholder="Date of Birth..." {...register("Date of Birth")}/>
+          <label>Date Of Birth: </label><input type='Number' placeholder="Date of Birth..." {...register("dateOfBirth")}/>
           
-          <p>{errors.dateOfbirthe?.message}</p>
+          <p>{errors.dateOfbirth?.message}</p>
 
           
-          <label>Country : </label> <select {...register("Country")}>
+          <label>Country : </label> <select {...register("country")}>
             <option>Nigeria</option>
             <option>South Africa</option>
             <option>America</option>
@@ -72,25 +54,22 @@ const{register, handleSubmit, formState: {errors}} = useForm({
           
           <br></br>
           
-          <label>State Of Origin: </label> <select {...register("State of Origin")}>
-          <option>Abia</option>
+          <label>State Of Origin: </label> <select {...register("stateOfOrigin")}>
+            <option>Abia</option>
             <option>Akwa Ibom</option>
             <option>Anambara</option>
             <option>Bauchi</option>
             <option>Bayelsa</option>
             <option>Imo </option>
           </select>
-          
-          
           <br></br>
-          
-          <label>Password: </label> <input type='text' placeholder="Password..." {...register("Password")}/>
-          
+          <label>Password: </label> <input type='text' placeholder="Password..." {...register("password")}/>
           <br></br>
-          <label>Confirm Password: </label> <input type='text' placeholder="Confirm password..." {...register("Confirm password")}/>
+          <label>Confirm Password: </label> <input type='text' placeholder="Confirm password..." {...register("confirmPassword")}/>
           <br></br>
-          <input type='submit' onSubmit={()=>handleSubmit(onSubmit)}/>
+          <input type='submit'/>
         </form>
+        <DevTool control={control}/>
       </div>
     </>
   )
