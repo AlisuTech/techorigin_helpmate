ee7508ec3c01ef8b3b7223773ef6482bc0bd5d64
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2024-06-06 17:08:18 +0100

    worked on the login form

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import './Login.css'
 
 const Login = () => {
+
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
+  const email = useRef()
+  const password = useRef()
+
+  function handleEmailFocus() {
+    const enteredEmail = email.current.value;
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+    } else {
+      setEmailIsInvalid(false);
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const enteredEmail = email.current.value;
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
+
+    console.log('Sending HTTP request...');
+  }
+
+
+
   return (
     <>
-        <div>Login</div>
+      <form onSubmit={handleSubmit}>
+        <div className="control-row">
+          <div className="control no-margin">
+            <label htmlFor="email">Email</label>
+            <input 
+              id="email" 
+              type="email" 
+              name="email" 
+              ref={email} 
+              onBlur={handleEmailFocus}
+            />
+            <div className="control-error">
+              {emailIsInvalid && <p>Please enter a valid email address.</p>}
+            </div>
+          </div>
+
+          <div className="control no-margin">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              ref={password}
+            />
+          </div>
+        </div>
+
+        <div className="form-actions">
+          <button className="button">Login</button>
+          <button className="button ">Register</button>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
 export default Login
