daaa457203cf2146c1e68a167b373d0ad2629d35
Joy-Nnadiegbulam <132944536+Joy-Nnadiegbulam@users.noreply.github.com>
2024-06-08 20:20:28 +0200

    Added CSS styling to the sign-up

--- src/pages/auth/signup/Signup.js
+++ src/pages/auth/signup/Signup.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { DevTool } from '@hookform/devtools';
 import { userSchema } from '../../../validations/userValidation';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,12 +10,12 @@ import { getCountriesAndCities } from '../../../services/countryService';
 import { useNavigate } from 'react-router-dom';
 
 
-function Signup () {
+function Signup() {
 
   const { status, error } = useSelector(state => state.user);
   const navigate = useNavigate()
   const dispatch = useDispatch();
-  const{register, handleSubmit, watch, control, formState: {errors}} = useForm({
+  const { register, handleSubmit, watch, control, formState: { errors } } = useForm({
     resolver: yupResolver(userSchema)
   });
   const [countries, setCountries] = useState([]);
@@ -37,70 +37,77 @@ function Signup () {
     fetchCountriesAndCities();
   }, []);
   const filteredCities = cities.filter(city => city.country === selectedCountry);
-  
- const onSubmit=(data)=>{
-      console.log(data)
-      dispatch(signupUser(data));
-      navigate('/login')
-    }
+
+  const onSubmit = (data) => {
+    console.log(data)
+    dispatch(signupUser(data));
+    navigate('/login')
+  }
   return (
     <>
       {status === 'loading' && <p>Loading...</p>}
       {status === 'succeeded' && <p>Sign up succeeded!</p>}
       {status === 'failed' && <p>Error: {error}</p>}
-      <div>
-        <form  onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor='firstName'>First Name: </label> <input type='text' id='firstName' placeholder="First Name..." {...register("firstName")}/>
+      <div className='bg-main'>
+        <div>
+          <form className='bg-form' onSubmit={handleSubmit(onSubmit)}>
           
-          <p>{errors.firstName?.message}</p>
-          
-          <label htmlFor='lastName'>Last Name: </label> <input type='text' id='lastName' placeholder="Last Name..." {...register("lastName")} />
-          
-          <p>{errors.lastName?.message}</p>
-          
-          <label>Phone Number: </label> <input type='Number' placeholder="Phone Number..." {...register("phoneNumber")} />
-          
-          <p>{errors.phoneNumber?.message}</p>
-          
-          <label>Email: </label> <input type='text' id='email' placeholder="Email..." {...register("email")}/>
-          
-          <p>{errors.email?.message}</p>
-          
-          <label>Office Address: </label> <input type='text' placeholder="Office address..." {...register("officeAddress")}/>
-         
-          <p>{errors.officeAddress?.message}</p>
-          
-          <label>Date Of Birth: </label><input type='Number' placeholder="Date of Birth..." {...register("dateOfBirth")}/>
-          
-          <p>{errors.dateOfbirth?.message}</p>
+            <p className='sign-up'> Sign up</p> <br></br>
+            <p className='create-account'>Create your account</p>
+            
+            <label htmlFor='firstName'>First Name: </label> <input className='place-holder' type='text' id='firstName' placeholder="First Name..." {...register("firstName")} />
 
-          
-          <label>Country : </label> <select {...register("country")}>
-          <option value="">Select Country</option>
-          {countries.map((country, index) => (
-            <option key={index} value={country}>{country}</option>
-          ))}
-          </select>
-          
-          <br></br>
-          
-          <label>State Of Origin: </label> <select {...register("stateOfOrigin")}>
-          <option value="">Select City</option>
-          {filteredCities.map((city, index) => (
-            <option key={index} value={city.city}>{city.city}</option>
-          ))}
-          </select>
+            <p>{errors.firstName?.message}</p>
 
-          <br></br>
+            <label htmlFor='lastName'>Last Name: </label> <input className='place-holder' type='text' id='lastName' placeholder="Last Name..." {...register("lastName")} />
 
-          <label>Password: </label> <input type='text' placeholder="Password..." {...register("password")}/>
-          <br></br>
-          <label>Confirm Password: </label> <input type='text' placeholder="Confirm password..." {...register("confirmPassword")}/>
-          <br></br>
-          <input type='submit'/>
-        </form>
-        <DevTool control={control}/>
+            <p>{errors.lastName?.message}</p>
+
+            <label>Phone Number: </label> <input className='place-holder' type='Number' placeholder="Phone Number..." {...register("phoneNumber")} />
+
+            <p>{errors.phoneNumber?.message}</p>
+
+            <label>Email: </label> <input className='place-holder' type='text' id='email' placeholder="Email..." {...register("email")} />
+
+            <p>{errors.email?.message}</p>
+
+            <label>Office Address: </label> <input className='place-holder' type='text' placeholder="Office address..." {...register("officeAddress")} />
+
+            <p>{errors.officeAddress?.message}</p>
+
+            <label>Date Of Birth: </label><input className='place-holder' type='Number' placeholder="Date of Birth..." {...register("dateOfBirth")} />
+
+            <p>{errors.dateOfbirth?.message}</p>
+
+
+            <label>Country : </label> <select className='place-holder' {...register("country")}>
+              <option  value="">Select Country</option>
+              {countries.map((country, index) => (
+                <option key={index} value={country}>{country}</option>
+              ))}
+            </select>
+
+            <br></br>
+
+            <label>State Of Origin: </label> <select className='place-holder' {...register("stateOfOrigin")}>
+              <option value="">Select City</option>
+              {filteredCities.map((city, index) => (
+                <option key={index} value={city.city}>{city.city}</option>
+              ))}
+            </select>
+
+            <br></br>
+
+            <label>Password: </label> <input className='place-holder' type='text' placeholder="Password..." {...register("password")} />
+            <br></br>
+            <label>Confirm Password: </label> <input className='place-holder' type='text' placeholder="Confirm password..." {...register("confirmPassword")} />
+            <br></br>
+            <input className='submit' type='submit' />
+          </form>
+          <DevTool control={control} />
+        </div>
       </div>
+
     </>
   )
 }
