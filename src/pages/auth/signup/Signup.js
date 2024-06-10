350f91f2c7c0b9c71001590d8df8e09d6a077d5a
jgina <168765609+jgina@users.noreply.github.com>
2024-06-10 17:37:07 +0100

    up dated sign up page

--- src/pages/auth/signup/Signup.js
+++ src/pages/auth/signup/Signup.js
@@ -1,32 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup'
-import { DevTool } from '@hookform/devtools';
-import { userSchema } from '../../../validations/userValidation';
-import { useDispatch, useSelector } from 'react-redux';
-import { signupUser } from '../../../app/user/userSlice';
-import './Signup.css'
-import { getCountriesAndCities } from '../../../services/countryService';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { DevTool } from "@hookform/devtools";
+import { userSchema } from "../../../validations/userValidation";
+import { useDispatch, useSelector } from "react-redux";
+import { signupUser } from "../../../app/user/userSlice";
+import "./Signup.css";
+import { getCountriesAndCities } from "../../../services/countryService";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
-
-  const { status, error } = useSelector(state => state.user);
-  const navigate = useNavigate()
+  const { status, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { register, handleSubmit, watch, control, formState: { errors } } = useForm({
-    resolver: yupResolver(userSchema)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(userSchema),
   });
   const [countries, setCountries] = useState([]);
   const [cities, setCities] = useState([]);
-  const selectedCountry = watch('country');
+  const selectedCountry = watch("country");
 
   useEffect(() => {
     const fetchCountriesAndCities = async () => {
       try {
         const data = await getCountriesAndCities();
-        const uniqueCountries = [...new Set(data.map(item => item.country))];
+        const uniqueCountries = [...new Set(data.map((item) => item.country))];
         setCountries(uniqueCountries);
         setCities(data);
       } catch (error) {
@@ -36,80 +40,119 @@ function Signup() {
 
     fetchCountriesAndCities();
   }, []);
-  const filteredCities = cities.filter(city => city.country === selectedCountry);
+  const filteredCities = cities.filter(
+    (city) => city.country === selectedCountry
+  );
 
   const onSubmit = (data) => {
-    console.log(data)
+    console.log(data);
     dispatch(signupUser(data));
-    navigate('/login')
-  }
+    navigate("/login");
+  };
   return (
     <>
-      {status === 'loading' && <p>Loading...</p>}
-      {status === 'succeeded' && <p>Sign up succeeded!</p>}
-      {status === 'failed' && <p>Error: {error}</p>}
-      <div className='bg-main'>
+      {status === "loading" && <p>Loading...</p>}
+      {status === "succeeded" && <p>Sign up succeeded!</p>}
+      {status === "failed" && <p>Error: {error}</p>}
+      <div className="bg-main">
         <div>
-          <form className='bg-form' onSubmit={handleSubmit(onSubmit)}>
-          
-            <p className='sign-up'> Sign up</p> <br></br>
-            <p className='create-account'>Create your account</p>
-            
-            <label htmlFor='firstName'>First Name: </label> <input className='place-holder' type='text' id='firstName' placeholder="First Name..." {...register("firstName")} />
-
+          <form className="bg-form" onSubmit={handleSubmit(onSubmit)}>
+            <p className="sign-up"> Sign up</p> <br></br>
+            <p className="create-account">Create your account</p>
+            <label htmlFor="firstName">First Name: </label>{" "}
+            <input
+              className="place-holder"
+              type="text"
+              id="firstName"
+              placeholder="First Name..."
+              {...register("firstName")}
+            />
             <p>{errors.firstName?.message}</p>
-
-            <label htmlFor='lastName'>Last Name: </label> <input className='place-holder' type='text' id='lastName' placeholder="Last Name..." {...register("lastName")} />
-
+            <label htmlFor="lastName">Last Name: </label>{" "}
+            <input
+              className="place-holder"
+              type="text"
+              id="lastName"
+              placeholder="Last Name..."
+              {...register("lastName")}
+            />
             <p>{errors.lastName?.message}</p>
-
-            <label>Phone Number: </label> <input className='place-holder' type='Number' placeholder="Phone Number..." {...register("phoneNumber")} />
-
+            <label>Phone Number: </label>{" "}
+            <input
+              className="place-holder"
+              type="Number"
+              placeholder="Phone Number..."
+              {...register("phoneNumber")}
+            />
             <p>{errors.phoneNumber?.message}</p>
-
-            <label>Email: </label> <input className='place-holder' type='text' id='email' placeholder="Email..." {...register("email")} />
-
+            <label>Email: </label>{" "}
+            <input
+              className="place-holder"
+              type="text"
+              id="Email"
+              placeholder="Email..."
+              {...register("Email")}
+            />
             <p>{errors.email?.message}</p>
-
-            <label>Office Address: </label> <input className='place-holder' type='text' placeholder="Office address..." {...register("officeAddress")} />
-
+            <label>Office Address: </label>{" "}
+            <input
+              className="place-holder"
+              type="text"
+              placeholder="Office address..."
+              {...register("officeAddress")}
+            />
             <p>{errors.officeAddress?.message}</p>
-
-            <label>Date Of Birth: </label><input className='place-holder' type='Number' placeholder="Date of Birth..." {...register("dateOfBirth")} />
-
+            <label>Date Of Birth: </label>
+            <input
+              className="place-holder"
+              type="Number"
+              placeholder="Date of Birth..."
+              {...register("dateOfBirth")}
+            />
             <p>{errors.dateOfbirth?.message}</p>
-
-
-            <label>Country : </label> <select className='place-holder' {...register("country")}>
-              <option  value="">Select Country</option>
+            <label>Country : </label>{" "}
+            <select className="place-holder" {...register("country")}>
+              <option value="">Select Country</option>
               {countries.map((country, index) => (
-                <option key={index} value={country}>{country}</option>
+                <option key={index} value={country}>
+                  {country}
+                </option>
               ))}
             </select>
-
             <br></br>
-
-            <label>State Of Origin: </label> <select className='place-holder' {...register("stateOfOrigin")}>
+            <label>State Of Origin: </label>{" "}
+            <select className="place-holder" {...register("stateOfOrigin")}>
               <option value="">Select City</option>
               {filteredCities.map((city, index) => (
-                <option key={index} value={city.city}>{city.city}</option>
+                <option key={index} value={city.city}>
+                  {city.city}
+                </option>
               ))}
             </select>
-
             <br></br>
-
-            <label>Password: </label> <input className='place-holder' type='text' placeholder="Password..." {...register("password")} />
+            <label>Password: </label>{" "}
+            <input
+              className="place-holder"
+              type="text"
+              placeholder="Password..."
+              {...register("password")}
+            />
             <br></br>
-            <label>Confirm Password: </label> <input className='place-holder' type='text' placeholder="Confirm password..." {...register("confirmPassword")} />
+            <label>Confirm Password: </label>{" "}
+            <input
+              className="place-holder"
+              type="text"
+              placeholder="Confirm password..."
+              {...register("confirmPassword")}
+            />
             <br></br>
-            <input className='submit' type='submit' />
+            <input className="submit" type="submit" />
           </form>
           <DevTool control={control} />
         </div>
       </div>
-
     </>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -1,16 +1,38 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 export const userSchema = yup.object().shape({
-
-    firstName: yup.string().required("Your first name is required"),
-    lastName: yup.string().required("Your last name is required"),
-    phoneNumber: yup.number().min(11).max(11).required("Your phone Number is not correct"),
-    email: yup.string().email().required("Your email is required"),
-    officeAddress: yup.string().required("Your office address is required"),
-    // dateOfbirth: yup.number().integer().min(16).max(200).required("Your age must be 16 and above"),
-    dateOfBirth: yup.number().integer().required("Your age must be 16 and above"),
-    country: yup.string().required("Your country is required"),
-    stateOfOrigin: yup.string().required("Your State is required"),
-    password: yup.string().min(4).max(30).required(),
-    confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Password dont match").required(),
-})
+  firstName: yup.string().required("Your first name is required"),
+  lastName: yup.string().required("Your last name is required"),
+  PhoneNumber: yup
+    .string()
+    .matches(/^\d{11}$/, "phone number must be 11 digits")
+    .required(),
+  Email: yup
+    .string()
+    .email("Pls use a valid email")
+    .required("Your email is required"),
+  officeAddress: yup.string().required("Your office address is required"),
+  dateOfbirth: yup.date("must be a date").required("pls enter your date of birth"),
+  Age: yup
+    .number()
+    .typeError("Age must be a number")
+    .integer()
+    .min(16, "you must be atleats 16 years old")
+    .max(100, "you cant be older than 100 years")
+    .required("Age is required"),
+  country: yup.string().required("Your country is required"),
+  stateOfOrigin: yup.string().required("Your State is required"),
+  Category: yup
+    .array()
+    .min(1, "select atleast one category")
+    .required("select atleast one category"),
+  Gender: yup.string().required(),
+  Password: yup
+    .string()
+    .required("password is required")
+    .min(5, "password must be atleast 8 characters"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "password must match")
+    .required(),
+});
