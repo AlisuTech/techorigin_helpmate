e8bfe3874cfab05f35466e3980a61b23a80ceab5
Techtory1 <vejere93@gmail.com>
2024-06-22 18:58:14 +0100

    I worked on the psychological page

--- src/pages/departments/psychological/Psychological.js
+++ src/pages/departments/psychological/Psychological.js
@@ -1,9 +1,207 @@
 import React from 'react'
-
+import "./Psychological.css"
 const Psychological = () => {
   return (
-    <div>Psychological</div>
+    <div>
+        <div class="background">
+            <div class=" font1 playfair-display-">
+                PSYCHOLOGICAL CARE
+                <br/>
+                <br/>
+                <p class="fonts2 playfair-display- textcolor">Helpmate: Doing Hard Things Can Build Character</p>
+                <p class="fonts2 playfair-display-">The best years of your life are the ones in <br><br>which you decide
+                    your problems are
+                    your own. <br/><br/>
+                    You do not blame them on your mother, the ecology,<br><br> or the president. You realize that you
+                    control your own destiny.
+                    <br><br> — ALBERT ELLIS
+                </p>
+                <button class="button3">Get Started</button>
+            </div>
+        </div>
+        <div class="backgrounddiv">
+            <div class="display ">
+                <img src="WhatsApp Image 2024-06-21 at 6.35.15 PM.jpeg" class="img1class">
+                <div class="maxwidth">
+                    <br>
+                    <p class="fonts2 playfair-display- "> “How should we live?”</p>
+                    <p class="fonts3 textcolor playfair-display-"><br><br>That is the question The Deep Sphere
+                        <br/><br/>tries to answer. To that aim,
+                        <br/><br/>we enter the place of culture and roam
+                        <br/><br/>through its room: psychology, philosophy,
+                        <br/><br/><br/>art, and science. Helpmate hopes to contribute to your calm,
+                        <br/><br>growth, healing, and self-understanding.
+                    </p>
+                    <br/><br><br>
+                    <p class="fonts3 textcolor playfair-display-"> We've got you covered!!!</p>
+
+                </div>
+
+            </div>
+            <br/>
+            <div class="Whatwedo font4 playfair-display-">
+                Services We Offer
+
+            </div>
+            <br/>
+            <div class="grid textcolor playfair-display-">
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.54 AM.jpeg" class="imageclass">
+                    <p class="fonts3">Pay attention to your problems</p><span><strong>Listening to all you've
+                            got to say with <br/>rapt attention and without judgement</strong></span></p>
+                </div>
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.57 AM.jpeg" class="imageclass">
+                    <p class="fonts3">Proper assesment and diagnosis</p><span><strong>Through Interviews, observation
+                            and<br>
+                            psychological tests, our
+                            professsionals
+                            can properly <br>and accurately diagnose the issue at hand</strong>
+                    </span></p>
+                </div>
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.58 AM (1).jpeg" class="imageclass">
+                    <p class="fonts3">Therapy and Councelling</p><span><strong>Providing councelling using evidence
+                            based<br> techniques
+                            to help clients
+                            overcome emotional, <br>behavioural and psychological issues</strong>
+                    </span></p>
+                </div>
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.58 AM.jpeg" class="imageclass">
+                    <p class="fonts3">Follow-Up and Supervision</p><span><strong>Supervising the client in order to<br>
+                            monitor their
+                            progress</strong></span></p>
+                </div>
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.59 AM.jpeg" class="imageclass">
+                    <p class="fonts3">Ethical and Legal Responsibilities</p><span><strong> Our Professionals are
+                            adequately
+                            trained <br>to
+                            ensure that
+                            everything<br>
+                            being discussed is kept safe and discreet.<br> Additionally, our Clients can choose to be
+                            completely
+                            <br>anonymous</strong>
+                    </span></p>
+                </div>
+            </div>
+            <br/>
+            <br/><br/>
+            <br/>
+            <br/>
+            <div>
+                <button class="buttons "><strong> Click Here <i class="fas fa-phone-volume icons fa-1x"></i></strong>
+                </button><span class="fonts3 textcolor playfair-display-"> To book an appointment with one of our
+                    Profesionals</span>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <br>
+            <div class="lastbuttondiv">
+                <button class="buttons2"> Home Page</button>
+            </div>
+        </div>
+    </div>   
   )
 }
 
 export default Psychological
+
+/**
+ * 
+ * <div>
+        <div class="background">
+            <div class=" font1 playfair-display-">
+                PSYCHOLOGICAL CARE
+                <br>
+                <br>
+                <p class="fonts2 playfair-display- textcolor">Helpmate: Doing Hard Things Can Build Character</p>
+                <p class="fonts2 playfair-display-">The best years of your life are the ones in <br><br>which you decide
+                    your problems are
+                    your own. <br><br>
+                    You do not blame them on your mother, the ecology,<br><br> or the president. You realize that you
+                    control your own destiny.
+                    <br><br> — ALBERT ELLIS
+                </p>
+                <button class="button3">Get Started</button>
+            </div>
+        </div>
+        <div class="backgrounddiv">
+            <div class="display ">
+                <img src="WhatsApp Image 2024-06-21 at 6.35.15 PM.jpeg" class="img1class">
+                <div class="maxwidth">
+                    <br>
+                    <p class="fonts2 playfair-display- "> “How should we live?”</p>
+                    <p class="fonts3 textcolor playfair-display-"><br><br>That is the question The Deep Sphere
+                        <br><br>tries to answer. To that aim,
+                        <br><br>we enter the place of culture and roam
+                        <br><br>through its room: psychology, philosophy,
+                        <br><br>art, and science. Helpmate hopes to contribute to your calm,
+                        <br><br>growth, healing, and self-understanding.
+                    </p>
+                    <br><br><br>
+                    <p class="fonts3 textcolor playfair-display-"> We've got you covered!!!</p>
+
+                </div>
+
+            </div>
+            <br>
+            <div class="Whatwedo font4 playfair-display-">
+                Services We Offer
+
+            </div>
+            <br>
+            <div class="grid textcolor playfair-display-">
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.54 AM.jpeg" class="imageclass">
+                    <p class="fonts3">Pay attention to your problems</p><span><strong>Listening to all you've
+                            got to say with <br>rapt attention and without judgement</strong></span></p>
+                </div>
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.57 AM.jpeg" class="imageclass">
+                    <p class="fonts3">Proper assesment and diagnosis</p><span><strong>Through Interviews, observation
+                            and<br>
+                            psychological tests, our
+                            professsionals
+                            can properly <br>and accurately diagnose the issue at hand</strong>
+                    </span></p>
+                </div>
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.58 AM (1).jpeg" class="imageclass">
+                    <p class="fonts3">Therapy and Councelling</p><span><strong>Providing councelling using evidence
+                            based<br> techniques
+                            to help clients
+                            overcome emotional, <br>behavioural and psychological issues</strong>
+                    </span></p>
+                </div>
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.58 AM.jpeg" class="imageclass">
+                    <p class="fonts3">Follow-Up and Supervision</p><span><strong>Supervising the client in order to<br>
+                            monitor their
+                            progress</strong></span></p>
+                </div>
+                <div class="childgrid"><img src="WhatsApp Image 2024-06-19 at 9.45.59 AM.jpeg" class="imageclass">
+                    <p class="fonts3">Ethical and Legal Responsibilities</p><span><strong> Our Professionals are
+                            adequately
+                            trained <br>to
+                            ensure that
+                            everything<br>
+                            being discussed is kept safe and discreet.<br> Additionally, our Clients can choose to be
+                            completely
+                            <br>anonymous</strong>
+                    </span></p>
+                </div>
+            </div>
+            <br>
+            <br><br>
+            <br>
+            <br>
+            <div>
+                <button class="buttons "><strong> Click Here <i class="fas fa-phone-volume icons fa-1x"></i></strong>
+                </button><span class="fonts3 textcolor playfair-display-"> To book an appointment with one of our
+                    Profesionals</span>
+            </div>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="lastbuttondiv">
+                <button class="buttons2"> Home Page</button>
+            </div>
+        </div>
+    </div>
+ * 
+ */
