9f50f50c45a25393d86b394d52732a3808822daf
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2024-06-24 10:20:05 +0100

    added more icons

--- src/components/navigationBar/NavigationBar.jsx
+++ src/components/navigationBar/NavigationBar.jsx
@@ -7,24 +7,51 @@ const  NavigationBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav>
+    <nav className="bg-[#3d52a0]">
       <Link to="/" className="title">
-        Website
+        HelpMate
       </Link>
       <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        {/* <span></span>
         <span></span>
-        <span></span>
-        <span></span>
+        <span></span> */}
+        <i class="ri-menu-line text-[35px]"></i>
+        {/* <i class="ri-close-line text-[35px]"></i> */}
       </div>
       <ul className={menuOpen ? "open" : ""}>
         <li>
-          <NavLink to="/about">About</NavLink>
+          <div className="dropdown border-yellowx w-[100%]">
+            <button 
+              className="dropdown-toggle border-greenx w-[100%] btn-link"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Departments
+            </button>
+            <ul className="dropdown-menu border-yellow space-y-1">
+                <li>
+                  <NavLink to="/medical" >
+                    Medical
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/careerMentorship" >
+                    Education
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/psychological">
+                    Psychological
+                  </NavLink>
+                </li>
+            </ul>
+          </div>
         </li>
         <li>
-          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/choice">Sign Up</NavLink>
         </li>
         <li>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/login">Sign In</NavLink>
         </li>
       </ul>
     </nav>
