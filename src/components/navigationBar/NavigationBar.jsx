0c4ba8b1921d72dfce9b88c556feaff3d9bd92cc
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2024-06-24 15:28:46 +0100

    working on the navbar

--- src/components/navigationBar/NavigationBar.jsx
+++ src/components/navigationBar/NavigationBar.jsx
@@ -1,61 +1,138 @@
 import React, { useState } from "react";
-
 import "./NavigationBar.css";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const  NavigationBar = () => {
+const NavigationBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-[#3d52a0]">
-      <Link to="/" className="title">
-        HelpMate
-      </Link>
-      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-        {/* <span></span>
-        <span></span>
-        <span></span> */}
-        <i class="ri-menu-line text-[35px]"></i>
-        {/* <i class="ri-close-line text-[35px]"></i> */}
-      </div>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <div className="dropdown border-yellowx w-[100%]">
-            <button 
-              className="dropdown-toggle border-greenx w-[100%] btn-link"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false">
-              Departments
-            </button>
-            <ul className="dropdown-menu border-yellow space-y-1">
+    <header className="header border-yellow">
+      <nav className="bg-[#3d52a0] nav nav_containerx border-red">
+        <div className="nav__data border-blue">
+          <Link to="/" className="nav__logo">
+            <i className="ri-home-7-line"></i> HelpMate
+          </Link>
+        </div>
+
+        <div
+          className="nav_toggle"
+          id="nav-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <i
+            className={`ri-menu-line nav__icon ${
+              menuOpen ? "nav__icon--hide" : ""
+            }`}
+          ></i>
+          <i
+            className={`ri-close-line nav__icon ${
+              menuOpen ? "" : "nav__icon--hide"
+            }`}
+          ></i>
+        </div>
+
+        <div
+          className={`${menuOpen ? "nav__menu" : "hidden"} border-green`}
+          id="nav-menu"
+        >
+          <ul className="nav__list border-yellow">
+            <li>
+              <Link to="/" className="nav__link">Home</Link>
+            </li>
+            <li>
+              <Link className="nav__link">Company</Link>
+            </li>
+
+            {/* DROPDOWN 1 */}
+            <li className="dropdown__item">
+              <div className="nav__link">
+                Analytics{" "}
+                <i className="ri-arrow-down-s-line dropdown__arrow "></i>
+              </div>
+
+              <ul className="dropdown__menu">
                 <li>
-                  <NavLink to="/medical" >
-                    Medical
-                  </NavLink>
+                  <Link className="dropdown__link">
+                    <i className="ri-pie-chart-line dropdown__arrow "></i>
+                    Overview
+                  </Link>
                 </li>
+
                 <li>
-                  <NavLink to="/careerMentorship" >
-                    Education
-                  </NavLink>
+                  <Link className="dropdown__link">
+                    <i className="ri-arrow-up-down-line"></i>
+                    Transactions
+                  </Link>
                 </li>
+
+                {/* DROPDOWN SUBMENU */}
+                <li className="dropdown__subitem">
+                  <div className="dropdown__link">
+                    <i className="ri-bar-chart-line"></i> Reports <i className="ri-add-line dropdown__add"></i>
+                  </div>
+
+                  <ul className="dropdown__submenu">
+                    <li>
+                      <Link className="dropdown__sublink">
+                        <i className="ri-file-list-line"></i> Documents
+                      </Link>
+                    </li>
+
+                    <li>
+                      <Link className="dropdown__sublink">
+                        <i className="ri-cash-line"></i> Payments
+                      </Link>
+                    </li>
+
+                    <li>
+                      <Link className="dropdown__sublink">
+                        <i className="ri-refund-2-line"></i> Refunds
+                      </Link>
+                    </li>
+
+                  </ul>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Link className="nav__link">Products</Link>
+            </li>
+
+            {/* DROPDOWN 2 */}
+            <li className="dropdown__item">
+              <div className="nav__link">
+                Users <i className="ri-arrow-down-s-line dropdown__arrow"></i>
+              </div>
+
+              <ul className="dropdown__menu">
                 <li>
-                  <NavLink to="/psychological">
-                    Psychological
-                  </NavLink>
+                  <Link className="dropdown__link">
+                    <i className="ri-user-line"></i> Profiles
+                  </Link>
                 </li>
-            </ul>
-          </div>
-        </li>
-        <li>
-          <NavLink to="/choice">Sign Up</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Sign In</NavLink>
-        </li>
-      </ul>
-    </nav>
+
+                <li>
+                  <Link className="dropdown__link">
+                    <i className="ri-lock-line"></i> Accounts
+                  </Link>
+                </li>
+
+                <li>
+                  <Link className="dropdown__link">
+                    <i className="ri-message-3-line"></i> Messages
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link className="nav__link">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 
-export default NavigationBar
+export default NavigationBar;
