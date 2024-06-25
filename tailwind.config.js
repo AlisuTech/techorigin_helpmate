49b138263ae8e6dfe3530c356ccc8d5813b25ad1
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2024-06-25 18:02:27 +0100

    updated the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: "Poppins",
+      },
+      colors: {
+        primary: "#3E82FC",
+        lightprimary: "#ECF3FF",
+      },
+      backgroundImage: {
+        Hero: "url('assets/background.png')",
+      },
+    },
   },
   plugins: [],
 }
