58b515581ac67fd789230d5646c89390600b7447
ihumsy <ekwuemeihuoma@gmail.com>
2024-06-29 21:13:24 +0100

    implemented homepage

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -5,95 +5,113 @@ const HomePage = () => {
   return (
     <>
 
-      <div className='margin overall-div'>
-        <div className='firsthomepagebg div-mainhome-opacity'>
-          <h1 className='alignfirsthomepage h1font'>&nbsp;HELP MATE</h1>
-          <div className='div2-firsthomepage'><h1 className='h3font'>Welcome!!!<br></br> to our safe <i class="fa fa-space-shuttle" aria-hidden="true"></i></h1></div>
-          <div className='alignspan'>
-            &nbsp;<span>&nbsp;Help mate believes in the power of compassion, personalized care to <br></br>
-              help you acheive medical & emotional wellbeing and enjoy a healthy lifestyle.
-            </span>
-            <br></br>
-            <p className='spanfont'>&nbsp;&nbsp;Our team of licensed health professionals, psychologist and therapist<br></br> are
-              dedicated to providing high quality, evidence based services to individuals, couples and
-              families
-            </p>
+<div className='margin overall-div'>
+             <div className='firsthomepagebg div-mainhome-opacity'>
+                <h1 className='alignfirsthomepage h1font'>&nbsp;HELP MATE</h1>
+                <div className='div2-firsthomepage'><h1 className='h3font'>Welcome!!!<br></br> to our safe <i class="fa fa-space-shuttle" aria-hidden="true"></i></h1></div>
+                <div className='alignspan'>
+                     &nbsp;<span>&nbsp;Help mate believes in the power of compassion, personalized care to <br></br>
+                    help you acheive medical & emotional wellbeing and enjoy a healthy lifestyle.
+                     </span>
+                    <br></br>
+                    <p className='spanfont'>&nbsp;&nbsp;Our team of licensed health professionals, psychologist and therapist<br></br> are 
+                    dedicated to providing high quality, evidence based services to individuals, couples and 
+                    families
+                    </p>
+                  
+                    <button className='homepagebutton'>Our services</button>
+                   
 
-            <button className='homepagebutton'>Our services</button>
-
-
-          </div>
-
-        </div>
-        <div className='div-med med-imag'>
-          <div className='alignpsycholgy'>
-            <h1 className='h1font'>&nbsp;&nbsp;MEDICAL</h1>
-            <div className='div-medical'>
-              <h3 className='h3font'>We provide professional advice for all body systems</h3>
-              <span className='spanfont'>Cardiovascular system</span>
-              <br></br>
-              <span className='spanfont'>Digestive system</span>
-              <br></br>
-              <span className='spanfont'>Immunology system</span>
-              <br></br>
-              <span className='spanfont'>Nervous system</span>
-              <br></br>
-              <span className='spanfont'>Reprodive system</span>
-              <br></br>
-              <span className='spanfont'>Respiratory system</span>
-              <br></br>
-              <span className='spanfont'>Skeletal system</span>
-              <br></br>
-              <span className='spanfont'>Sensory system</span>
-              <br></br>
-              <span className='spanfont'>Urinary system</span>
-              <br></br>
-              <br></br>
-              <button className='medicalbutton'><p>Check our services</p></button>
+                </div>
+               
+            </div>
+            <div className='div-med med-imag'>
+                <div className='alignpsycholgy'>
+                    <h1 className='h1font'>&nbsp;&nbsp;MEDICAL</h1>
+                </div>
+                <div className='div-medical'>
+                   <h3 className='h3font'> &nbsp;&nbsp;We provide professional advice for all body systems</h3>
+                </div>
+                <div className='div-medicalSystems2'>
+                  <span className='spanfontmedical'>Cardiovascular system</span>
+                </div>
+                <div className='div-medicalSystems3'>
+                    <p className='spanfontmedical'>Digestive System</p>
+                </div>
+                <div className='div-medicalSystems4'>
+                    <p className='spanfontmedical'>Immunology System</p>
+                </div>
+                <div className='div-medicalSystems5'>
+                    <p className='spanfontmedical'>Nervous System</p>
+                </div>
+                <div className='div-medicalSystems1'>
+                    <p className='spanfontmedical'>Reproductive System</p>
+                </div>
+                <div className='div-medicalSystems6'>
+                    <p className='spontfontmedical'>Respiratory System</p>
+                </div>
+                <div className='div-medicalSystems7'>
+                    <p className='spanfontmedical'>Skeletal System</p>
+                </div>
+                <div className='div-medicalSystems8'>
+                    <p className='spanfontmedical'>Sensory System</p>
+                </div>
+                <div className='div-medicalSystems9'>
+                    <p className='spanfontmedical'>Urinary System</p>
+                </div>
+                <br></br>
+                <div>
+                  <button className='medicalbutton'><p>Check our services</p></button>
+                </div>
 
             </div>
+            <div className="div-psy psy-imag">
+                <div className='alignpsychology'>
+                    <h1 className='h1font'>&nbsp;PSYCHOLOGY</h1>
+                    <div className='div-psychology'>
+                          <h3 className='h3font'>We care about your psychological health</h3>
+                          <span className='spanfont'>Mental health</span>
+                         <br></br>
+                         <span className='spanfont'>Sexual therapy</span>
+                         <br></br>
+                         <span className='spanfont'>Marriage and family counselling</span>
+                         <br></br>
+                         <br></br>
+                         <button className='psychologybutton'>Check Us Out</button>
 
-          </div>
-        </div>
-        <div className="div-psy psy-imag">
-          <div className='alignpsychology'>
-            <h1 className='h1font'>&nbsp;PSYCHOLOGY</h1>
-            <div className='div-psychology'>
-              <h3 className='h3font'>We care about your psychological health</h3>
-              <span className='spanfont'>Mental health</span>
-              <br></br>
-              <span className='spanfont'>Sexual therapy</span>
-              <br></br>
-              <span className='spanfont'>Marriage and family counselling</span>
-              <br></br>
-              <br></br>
-              <button className='psychologybutton'>Check Us Out</button>
+                    </div>
+                </div>
 
             </div>
-          </div>
+            <div className='finance-imag alignfinance'>
+                <h1 className='h1font'>FINANCE</h1>
+                <div className='divfinance2'>
+                      <h3 className='h3font'>We offer guides on how to make good financial decisions</h3>
+                      <button className='financebutton'>Click to see more</button>
 
+                </div>
+            </div>
+           
+            <div className='lifestylef'>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <br></br><br></br><br></br><br></br>
+
+                <div className='divlifestyle2'>
+                   <h3 className='lifestylefont'>CAREER</h3>
+                   <h3 className='lifestylefont'>EAT HEALTHY</h3>
+                   <h3 className='lifestylefont'>FASHION</h3>
+                   <h3 className='lifestylefont'>FITNESS</h3>
+                   <button className='lifestylebutton'>Click to see more</button>
+                
+
+                </div>
+                
+                
+            </div>
+           
+           
         </div>
-        <div className='finance-imag alignfinance'>
-          <h1 className='h1font'>FINANCE</h1>
-          <div className='divfinance2'>
-            <h3 className='h3font'>We offer guides on how to make good financial decisions</h3>
-            <button className='financebutton'>Click to see more</button>
-
-          </div>
-        </div>
-        <div className='lifestyleflex'>
-          <div className='lifestlyeflex'>
-            <div className='fitness-imag lifestylediv'><p className='lifestylepadding h3font'><b>Stay fit</b></p></div>
-            <div className='diet-imag lifestylediv'><p className='lifestylepadding h3font'><b>EAt Healthy</b></p></div>
-            <div className='fashion-imag lifestylediv'><p className='lifestylepadding h3font'><b>Look Good</b></p></div>
-            <div className='carrrr-imag lifestylediv'><p className='lifestylepadding h3font'><b>Work Hard</b></p></div>
-            <div className='divfinance2'></div>
-          </div>
-
-        </div>
-        <div className='notelifestyle'>f</div>
-
-      </div>
 
 
 
