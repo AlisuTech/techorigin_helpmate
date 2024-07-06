e1db944fbffa1ac0b242b5dec9acbfe3b6d75d67
giddy11 <edoghotugiddy@gmail.com>
2024-07-06 12:59:43 +0100

    changed the details of the footer

--- src/tests/Test.js
+++ src/tests/Test.js
@@ -13,3 +13,41 @@ function Test() {
 }
 
 export default Test;
+
+// export const SERVICES = [
+//   { name: "Medical Appointments", link: "#" },
+//   { name: "Wellness Sessions", link: "#" },
+//   { name: "Educational Tutoring", link: "#" },
+//   { name: "Therapy and Counseling", link: "#" },
+// ];
+
+// export const RESOURCES = [
+//   { name: "User Guides", link: "#" },
+//   { name: "Service Provider FAQs", link: "#" },
+//   { name: "Blog", link: "#" },
+//   { name: "Success Stories", link: "#" },
+//   { name: "Community Events", link: "#" },
+// ];
+
+// export const COMPANY = [
+//   { name: "About Us", link: "#" },
+//   { name: "Our Team", link: "#" },
+//   { name: "Press", link: "#" },
+//   { name: "Careers", link: "#" },
+//   { name: "Contact Us", link: "#" },
+// ];
+
+// export const SUPPORT = [
+//   { name: "Help Center", link: "#" },
+//   { name: "Tutorials & Guides", link: "#" },
+//   { name: "Webinars", link: "#" },
+//   { name: "Privacy Policy", link: "#" },
+// ];
+
+// export const SOCIAL_MEDIA = [
+//   { name: "logo-facebook", link: "#" },
+//   { name: "logo-twitter", link: "#" },
+//   { name: "logo-github", link: "#" },
+//   { name: "logo-linkedin", link: "#" },
+//   { name: "logo-instagram", link: "#" },
+// ];
