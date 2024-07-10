2050fede8c50b99a16b9686ff6733448a59397b3
jgina <168765609+jgina@users.noreply.github.com>
2024-07-10 12:15:14 +0100

    update

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 const Login = () => {
   const [email, emailChanged] = useState('')
   const [password, passwordChanged] = useState('')
+  const [error, setError] = useState('');
 
   return (
     <div>
