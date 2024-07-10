79c72665e22ac6a12d58f62e53d1be186b04f35b
Barizorn <112787981+Barizorn@users.noreply.github.com>
2024-07-10 05:50:37 +0100

    medical responsiveness v1.1

--- src/pages/departments/medical/Medical.js
+++ src/pages/departments/medical/Medical.js
@@ -7,6 +7,9 @@ import doc_1 from "../../../assets/medicalPage/doc_1.png";
 import doc_2 from "../../../assets/medicalPage/Doc_2.png";
 import doc_3 from "../../../assets/medicalPage/Doc_3.png";
 import { Col, Container, Row } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
+// import Carousel from "react-bootstrap/Carousel";
+// import ExampleCarouselImage from "components/ExampleCarouselImage";
 
 const Medical = () => {
   return (
@@ -32,26 +35,84 @@ const Medical = () => {
         </div>
 
         {/* <div class="div1">
-        <table>
-          <tr>
-            <td>
-              <div class="table_row1">
-                <span class="div1_icons">
-                  {" "}
+          <table>
+            <tr>
+              <td>
+                <div class="table_row1">
+                  <p class="div1_icons">
+                    <i class="fa fa-sticky-note"></i>
+                  </p>
+                  <div>
+                    <p class="div1_bold">100% Confidential</p>
+                    <div class="div1_light">
+                      <p>
+                        Discover the unseen potential of HelpMate, <br />
+                        where discretion meets innovation.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <div class="table_row2_3">
+                  <span class="div1_icons">
+                    <i class="fa fa-users"></i>
+                  </span>
+                  <div>
+                    <span class="div1_bold">Qualified Team</span>
+                    <br></br>
+                    <div class="div1_light">
+                      <span>Experience exceptional treatment</span>
+                      <span>
+                        with our team of dedicated, quality doctors. Your
+                        health,
+                      </span>
+                      <span>our priority!</span>
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <div class="table_row2_3">
+                  <span class="div1_icons">
+                    <i class="fa fa-umbrella"></i>
+                  </span>
+                  <div>
+                    <span class="div1_bold">Book an Appointment</span>
+                    <br></br>
+                    <div class="div1_light">
+                      <span>Go through the seamless process</span>
+                      <span>
+                        of booking an appointment with our user-friendly
+                      </span>
+                      <span>website.</span>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div> */}
+        <div className="green-row-div">
+          <Row className="green-row-div">
+            <Col sm={12} md={6} lg={3} xl={3} className="div-col1-bg">
+              <div className="table-row-flex">
+                <p class="div1_icons">
                   <i class="fa fa-sticky-note"></i>
-                </span>
+                </p>
                 <div>
-                  <span class="div1_bold">100% Confidential</span> <br></br>
+                  <p class="div1_bold">100% Confidential</p>
                   <div class="div1_light">
-                    <span>Discover the unseen potential of HelpMate,</span>
-                    <br />
-                    <span>where discretion meets innovation.</span>
+                    <p>
+                      Discover the unseen potential of HelpMate, <br />
+                      where discretion meets innovation.
+                    </p>
                   </div>
                 </div>
               </div>
-            </td>
-            <td>
-              <div class="table_row2_3">
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={4} className="div-col2-bg">
+              <div className="table-row-flex">
                 <span class="div1_icons">
                   <i class="fa fa-users"></i>
                 </span>
@@ -67,9 +128,9 @@ const Medical = () => {
                   </div>
                 </div>
               </div>
-            </td>
-            <td>
-              <div class="table_row2_3">
+            </Col>
+            <Col sm={12} md={12} lg={4} xl={4} className="div-col3-bg">
+              <div className="table-row-flex">
                 <span class="div1_icons">
                   <i class="fa fa-umbrella"></i>
                 </span>
@@ -85,10 +146,9 @@ const Medical = () => {
                   </div>
                 </div>
               </div>
-            </td>
-          </tr>
-        </table>
-      </div> */}
+            </Col>
+          </Row>
+        </div>
 
         <div className="our-department-main-div">
           <div class="department">
@@ -102,11 +162,11 @@ const Medical = () => {
             </p>
           </div>
 
-          <Row>
-            <Col sm={12} md={4} lg={4} xl={3}>
+          <Row className="department-div-row">
+            <Col sm={12} md={4} lg={4} xl={4}>
               <div className="department-column">
-                <div>
-                  <img src={relation} alt="" class="department-image" />
+                <div className="department-image-div">
+                  <img src={relation} alt="" className="department-image" />
                 </div>
                 <div>
                   <p class="department-text1">General Clinic</p>
@@ -119,7 +179,7 @@ const Medical = () => {
             </Col>
             <Col sm={12} md={4} lg={4} xl={3}>
               <div className="department-column">
-                <div>
+                <div className="department-image-div">
                   <img src={family} alt="" class="department-image" />
                 </div>
                 <div>
@@ -134,7 +194,7 @@ const Medical = () => {
             </Col>
             <Col sm={12} md={4} lg={4} xl={3}>
               <div className="department-column">
-                <div>
+                <div className="department-image-div">
                   <img src={depression} alt="" class="department-image" />
                 </div>
                 <div>
@@ -162,9 +222,49 @@ const Medical = () => {
             <span class="underline"></span>
           </div>
 
-          <Row>
+          <div className="medical-carousel-div">
+            <Carousel className="medical-carousel">
+              <Carousel.Item className="medical-carousel-item">
+                <img
+                  className="medical-carousel-image"
+                  src={doc_1}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <div>
+                    <p class="department-text1">Dr. Chimuanya Oji</p>
+                    <p class="department-text2">Physician</p>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="medical-carousel-item">
+                <img
+                  className="medical-carousel-image"
+                  src={doc_2}
+                  alt="Second slide"
+                />
+                <Carousel.Caption>
+                  <p class="department-text1">Dr Steeze Hundred</p>
+                  <p class="department-text2">Pediatrician</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="medical-carousel-item">
+                <img
+                  className="medical-carousel-image"
+                  src={doc_3}
+                  alt="Third slide"
+                />
+                <Carousel.Caption>
+                  <p class="department-text1">Dr Composure Thanks</p>
+                  <p class="department-text2">Gynaecologist</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+
+          {/* <Row>
             <Col sm={12} md={4} lg={4} xl={3}>
-              <div>
+              <div className="department-image-div">
                 <img src={doc_1} alt="" class="department-image" />
               </div>
               <div>
@@ -173,7 +273,7 @@ const Medical = () => {
               </div>
             </Col>
             <Col sm={12} md={4} lg={4} xl={3}>
-              <div>
+              <div className="department-image-div">
                 <img src={doc_2} alt="" class="department-image" />
               </div>
               <div>
@@ -182,7 +282,7 @@ const Medical = () => {
               </div>
             </Col>
             <Col sm={12} md={4} lg={4} xl={3}>
-              <div>
+              <div className="department-image-div">
                 <img src={doc_3} alt="" class="department-image" />
               </div>
               <div>
@@ -190,8 +290,7 @@ const Medical = () => {
                 <p class="department-text2">Gynaecologist</p>
               </div>
             </Col>
-          </Row>
-
+          </Row> */}
           {/* <div>
           <div class="bg-department">
             <div>
@@ -202,18 +301,17 @@ const Medical = () => {
             </div>
           </div>
         </div> */}
+        </div>
 
-          <div class="bg-client image-fluid">
-            <div class="client-text1 space-y-5">
-              <span>
-                <i class="fa fa-quote-left"></i> <br />I was able to finally
-                open up to <br /> someone about some very sensitive <br />{" "}
-                information about my body, and now <br /> I am getting all the
-                help I need!
-              </span>
-              <div class="underline2"></div>
-              <span class="client-text2">Adoa Adam</span>
-            </div>
+        <div class="bg-client image-fluid">
+          <div class="client-text1 space-y-5">
+            <span>
+              <i class="fa fa-quote-left"></i> <br />I was able to finally open
+              up to <br /> someone about some very sensitive <br /> information
+              about my body, and now <br /> I am getting all the help I need!
+            </span>
+            <div class="underline2"></div>
+            <span class="client-text2">Adoa Adam</span>
           </div>
         </div>
 
